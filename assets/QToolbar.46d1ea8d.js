8079056037bd9952f39b1a9de713bf4740102443
Galih Prambudiarta <42970402+prambudiarta@users.noreply.github.com>
2024-08-12 15:25:30 +0700

    New Build: TS

---
+++ assets/QToolbar.46d1ea8d.js
@@ -0,0 +1,1 @@
+import{c as t,h as a}from"./render.f2fea035.js";import{c as s,h as l}from"./index.5079f055.js";var c=t({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:e}){const r=s(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>l("div",{class:r.value,role:"toolbar"},a(e.default))}});export{c as Q};
---
+++ assets/interfaces.86b2316d.js
@@ -0,0 +1,1 @@
+var l=(e=>(e.Ongoing="ongoing",e.Completed="completed",e.Cancelled="cancelled",e.CloseRequest="close_request",e))(l||{});export{l as O};
